3840870f0a0d2fef2b68fd5d224a838499607cea
X <113698367+xiaojinghan2022@users.noreply.github.com>
2023-07-06 22:17:43 +0800

    一个小小的尝试

--- index.js
+++ index.js
@@ -1,61 +1,117 @@
-var ipUrl = "http://ip-api.com/json/?fields=66846719";
 var xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        var response = JSON.parse(xhr.response);
-        
-        if (response.status !== 'success') {
-            console.log("url连接成功，但"+response.message);
-            return
-        };
-        
-    
-        if (response.countryCode == "US") {
-            window.location.replace("https://google.com/")
-        };
-        if (response.countryCode == "CA") {
-            window.location.replace("https://google.ca/")
-        };
-        console.log(response);
-        
-        var Ip = document.getElementById("ip");
-        
-        Ip.textContent = '当前IP为:'+response.query;
-        console.log('你的IP为:'+response.query);
-        console.log('IP位于：'+response.country+" "+response.regionName);
-    }
-}
+var ipUrl = "https://ipapi.co/json/";
 
 xhr.open('GET',ipUrl,true);
 xhr.send();
 
-xhr.onerror = function () {
-    console.error('connect time out');
     
-    var ipUrl = "https://ipapi.co/json/";
-    var xhr = new XMLHttpRequest();
-    
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var response = JSON.parse(xhr.response);
+            
+        if (response.region_code == "US") {
+            window.location.replace("https://google.com/")
+        };
+        if (response.region_code == "CA") {
+            window.location.replace("https://google.ca/")
+        };
+            
+        console.log(response);
+        
+        var Ip = document.getElementById("ip");
+        
+        Ip.textContent = '当前IP为:'+response.ip+" ("+response.version+")"
+        console.log('你的'+response.version+'地址为:'+response.ip)
+        console.log('IP位于：'+response.country+" "+response.city)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**try {
+    var ipUrl = "http://ip-api.com/json/?fields=66846719";
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var response = JSON.parse(xhr.response);
-            
-            if (response.region_code == "US") {
+        
+            if (response.status !== 'success') {
+                console.log("url连接成功，但"+response.message);
+                return
+            };
+        
+    
+            if (response.countryCode == "US") {
                 window.location.replace("https://google.com/")
             };
-            if (response.region_code == "CA") {
+            if (response.countryCode == "CA") {
                 window.location.replace("https://google.ca/")
             };
-            
             console.log(response);
         
             var Ip = document.getElementById("ip");
         
-            Ip.textContent = '当前IP为:'+response.ip+" ("+response.version+")"
-            console.log('你的'+response.version+'地址为:'+response.ip)
-            console.log('IP位于：'+response.country+" "+response.city)
+            Ip.textContent = '当前IP为:'+response.query;
+            console.log('你的IP为:'+response.query);
+            console.log('IP位于：'+response.country+" "+response.regionName);
         }
     }
-    xhr.open('GET',ipUrl,true);
-    xhr.send();
+
+} catch (error) {
+    xhr.onerror = function () {
+        console.error('connect time out');
+    
+        var ipUrl = "https://ipapi.co/json/";
+    
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var response = JSON.parse(xhr.response);
+            
+                if (response.region_code == "US") {
+                    window.location.replace("https://google.com/")
+                };
+                if (response.region_code == "CA") {
+                    window.location.replace("https://google.ca/")
+                };
+            
+                console.log(response);
+        
+                var Ip = document.getElementById("ip");
+        
+                Ip.textContent = '当前IP为:'+response.ip+" ("+response.version+")"
+                console.log('你的'+response.version+'地址为:'+response.ip)
+                console.log('IP位于：'+response.country+" "+response.city)
+            }
+        }
+    }
 }
+xhr.open('GET',ipUrl,true);
+xhr.send();*/
