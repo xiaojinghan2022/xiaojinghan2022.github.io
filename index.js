2c79f63a22def2d4d245bc46051d67108d28c67c
X <113698367+xiaojinghan2022@users.noreply.github.com>
2023-07-06 18:14:52 +0800

    modified:   index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var ipUrl = "http://ip-api.com/json/?fields=66846719";
-var ipUrl = "https://ipapi.co/json/";
+
 var xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function () {
@@ -8,24 +8,42 @@ xhr.onreadystatechange = function () {
         
         if (response.status !== 'success') {
             console.log("url连接成功，但"+response.message);
+            
+            var ipUrl = "https://ipapi.co/json/";
+            if (response.region_code == "US") {
+                window.location.replace("https://google.com/")
+            };
+            if (response.region_code == "CA") {
+                window.location.replace("https://google.ca/")
+            };
+            
+            console.log(response);
+        
+            var Ip = document.getElementById("ip");
+        
+            ip.textContent = '当前IP为:'+response.ip+" ("+response.version+")"
+            console.log('你的'+response.version+'地址为:'+response.ip)
+            console.log('IP位于：'+response.country+" "+response.city)
             return
         };
         
-            if (response.region_code == "US") {
+    
+        if (response.countryCode == "US") {
             window.location.replace("https://google.com/")
-        }
-        if (response.region_code == "CA") {
+        };
+        if (response.countryCode == "CA") {
             window.location.replace("https://google.ca/")
-        }
+        };
         console.log(response);
         
-        var Ip = document.getElementById("ip")
+        var Ip = document.getElementById("ip");
         
-        ip.textContent = '当前IP为:'+response.ip+" ("+response.version+")"
-        console.log('你的'+response.version+'地址为:'+response.ip)
-        console.log('IP位于：'+response.country+" "+response.city)
+        ip.textContent = '当前IP为:'+response.query;
+        console.log('你的IP为:'+response.query);
+        console.log('IP位于：'+response.country+" "+response.regionName);
     }
 }
+
 xhr.open('GET',ipUrl,true);
 xhr.send();
 
