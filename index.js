402c4a7ec342df34e66cb69e1e92e6a50673e12d
X <113698367+xiaojinghan2022@users.noreply.github.com>
2023-07-06 22:39:42 +0800

    modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ xhr.onreadystatechange = function () {
         console.log(response);
         
         var Ip = document.getElementById("ip");
+        var City = document.getElementById("city");
         
         Ip.textContent = '当前IP为:'+response.ip+" ("+response.version+")"
+        City.textContent = 'IP属地为:'+response.country_name+response.city
         console.log('你的'+response.version+'地址为:'+response.ip)
         console.log('IP位于：'+response.country_name+" "+response.city)
     }
