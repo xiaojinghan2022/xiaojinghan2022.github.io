b5096f593c974cd8b8ec35caee28ea9ddc0a65dd
X <113698367+xiaojinghan2022@users.noreply.github.com>
2023-07-08 07:35:29 +0800

    modified:   config/href.js 	modified:   index.js

--- config/href.js
+++ config/href.js
@@ -1,4 +1,6 @@
 import { path } from "./path";
+import { path_language } from "..";
 //to ./index.html
 document.getElementById("CN").href = path+"CN/";
 document.getElementById("US").href = path+"US/";
+console.log(path_language)
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { path } from "./config/path";
 
 var xhr = new XMLHttpRequest();
-var path_language;
+//var path_language;
 xhr.open('GET',"https://ipapi.co/json/",true);
 xhr.send();
 xhr.onreadystatechange = function () {
@@ -21,30 +21,35 @@ xhr.onreadystatechange = function () {
         Region.textContent = 'IP属地为:'+response.country_name+" "+response.region;
         var path_language = "https://xiaojinghan2022.github.io/"+response.languages;
         console.log(path_language)
+        window.path_language=path_language
         var lat= "&lat="+response.latitude
         var lng= "lng="+response.longitude
-        xhr.open('GET',"https://res.abeim.cn/api-location_geocoder_address?"+lng+lat,true);
-        xhr.send();
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var response = JSON.parse(xhr.response);
-                var ipPlace = document.getElementById("place")
-                var ipAddress = response.data
-                if(response.code !== 200) {
-                    ipPlace.textContent = 'Error: ' + response.msg;
-                    return
-                };
-                if(ipAddress.city === undefined) {
-                    ipPlace.textContent = '你可能在 ' + ipAddress.recommend
-                    return
-                };
-                ipPlace.textContent = ' '+ipAddress.city+
-                                ' '+ipAddress.district+
-                                ' '+ipAddress.street+
-                                ' '+ipAddress.title
+        try {
+            xhr.open('GET',"https://res.abeim.cn/api-location_geocoder_address?"+lng+lat,true);
+            xhr.send();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var response = JSON.parse(xhr.response);
+                    var ipPlace = document.getElementById("place")
+                    var ipAddress = response.data
+                    if(response.code !== 200) {
+                        ipPlace.textContent = 'Error: ' + response.msg;
+                        return
+                    };
+                    if(ipAddress.city === undefined) {
+                        ipPlace.textContent = '你可能在 ' + ipAddress.recommend
+                        return
+                    };
+                    ipPlace.textContent = ' '+ipAddress.city+
+                                    ' '+ipAddress.district+
+                                    ' '+ipAddress.street+
+                                    ' '+ipAddress.title
 
+                }
             }
-        }
+        }catch (error){
+            document.getElementById("place").textContent = '你在 ' +response.longitude+","+response.latitude
+        };
     }
 };
 export {
