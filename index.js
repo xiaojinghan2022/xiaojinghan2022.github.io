f6e3c43e41451f83c910807d07aa4a2a516b9305
X <113698367+xiaojinghan2022@users.noreply.github.com>
2023-07-08 00:54:52 +0800

    renamed:    en_US/index.html -> CN/index.html 	renamed:    en_US/index.js -> CN/index.js 	renamed:    zh_CN/index.html -> US/index.html 	renamed:    zh_CN/index.js -> US/index.js 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var xhr = new XMLHttpRequest();
-
 xhr.open('GET',"https://ipapi.co/json/",true);
 xhr.send();
-
-    
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
         var response = JSON.parse(xhr.response);
@@ -14,13 +11,35 @@ xhr.onreadystatechange = function () {
         if (response.country_code == "CN") {
             window.location.replace("./zh_CN")
         };
-            
-        console.log(response);
         
         var Ip = document.getElementById("ip");
-        var City = document.getElementById("city");
+        var Region = document.getElementById("region");
         
-        Ip.textContent = '当前IP为:'+response.ip+" ("+response.version+")"
-        City.textContent = 'IP属地为:'+response.country_name+" "+response.city
+        Ip.textContent = '当前IP为:'+response.ip+" ("+response.version+")";
+        Region.textContent = 'IP属地为:'+response.country_name+" "+response.region;
+        var path_language = "https://xiaojinghan2022.github.io/"+response.language;
+        console.log(path_language)
+        var lat = "&lat="+response.latitude
+        var lng = "lng="+response.longitude
+        
+    }
+    window.lat = lat
+    window.lng = lng
+};
+console.log(lat);
+console.log(lng);
+var du_fanyi;
+
+xhr.open('GET',"http://res.abeim.cn/api-location_geocoder_address?"+lng+lat,true);
+xhr.send();
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var response = JSON.parse(xhr.response);
+
+        var ipPlace = document.getElementById("place")
+        ipPlace.textContent = ' '+response.city+
+            ' '+response.district+
+            ' '+response.street+
+            ' '+response.title
     }
 }
