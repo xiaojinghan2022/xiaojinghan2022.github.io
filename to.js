f367bac8435a2169b834e10d3f7a3d851684bc6d
X <113698367+xiaojinghan2022@users.noreply.github.com>
2023-07-17 21:35:03 +0800

    modified:   to.js

---
+++ to.js
@@ -0,0 +1,4 @@
+if(   window.navigator.systemLanguage=="zh-cn"   )   
+    window.location="./Tianxing/zh-CN/index.html";   
+if(   window.navigator.systemLanguage=="en-us"   )   
+    window.location="./Tianxing/en-US/index.html";   
