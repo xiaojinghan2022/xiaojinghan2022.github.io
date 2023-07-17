2cc8084cdb5a6ef6d216a79b223c371ccbb5bf89
X <113698367+xiaojinghan2022@users.noreply.github.com>
2023-07-17 19:33:20 +0800

    重置

--- Tianxing/index.js
+++ Tianxing/index.js
@@ -1,14 +1,13 @@
-
 var xhr = new XMLHttpRequest();
 //var path_language;
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
         var response = JSON.parse(xhr.response);
         if (response.country_code == "US") {
-            window.location.replace("./US")
+            window.location.replace("./en-US")
         };
         if (response.country_code == "CN") {
-            window.location.replace("./CN")
+            window.location.replace("./zh-CN")
         };
         
         var Ip = document.getElementById("ip");
