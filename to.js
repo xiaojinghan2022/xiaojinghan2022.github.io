1a5d8470f3abccb7c348d4e993e97b950292384e
X <113698367+xiaojinghan2022@users.noreply.github.com>
2023-07-18 22:12:49 +0800

    rebuild

--- to.js
+++ to.js
@@ -1,4 +1,4 @@
-if(   window.navigator.systemLanguage=="zh-cn"   )   
-    window.location="./Tianxing/zh-CN/index.html";   
-if(   window.navigator.systemLanguage=="en-us"   )   
-    window.location="./Tianxing/en-US/index.html";   
+var lang = navigator.language || navigator.userLanguage;
+        if(lang.substr(0, 3) == "zh-") location = "./Tianxing/zh-CN";
+        else if(lang.substr(0, 2) == "en") location = "./Tianxing/en-US";	
+        else location = "./Tianxing";
